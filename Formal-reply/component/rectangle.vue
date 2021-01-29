d7edc4496c75de4cca8daf196b6f44a9d29919df
GGtb <474714440@qq.com>
2021-01-29 16:41:24 +0800

    1.29

--- Formal-reply/component/rectangle.vue
+++ Formal-reply/component/rectangle.vue
@@ -1,8 +1,33 @@
 <template>
-	
+	<view class="rectangle">
+		<view v-for="(item,index) in dataList" :key="index">
+			<view class="rectangleLeft">
+				
+			</view>
+			<view class="rectangleRight">
+				
+			</view>
+		</view>
+	</view>
 </template>
 
 <script>
+	export default{
+		data(){
+			return{
+				imgUrl:this.$imgUrl
+			}
+		},
+		// props:["content","type"],
+		props:{
+			dataList:{
+				type:Array
+			},
+		},
+		methods:{
+
+		}
+	}
 </script>
 
 <style>
